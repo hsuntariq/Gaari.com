1a0a979a181d6b5c720379f08c1aaef0aada28d4
unknown <hsuntariq@gmail.com>
2024-08-24 18:00:27 +0500

    updates

--- client/src/pages/Collections.jsx
+++ client/src/pages/Collections.jsx
@@ -91,7 +91,6 @@ const Collections = () => {
                               d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
                             ></path>
                           </svg>
-
                           <svg
                             class="button__icon-svg  button__icon-svg--copy"
                             xmlns="http://www.w3.org/2000/svg"
